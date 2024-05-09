b7d284d927f00436fa32d7e940175c1f37a6e167
Marcio Kleber Junior Lopes de Souza <marciokjls@gmail.com>
2024-05-09 16:47:24 -0300

    Captura e save da foto no cache da aplicação.

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { PhotoService } from '../services/photo.service';
+
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +9,10 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(public photoService: PhotoService) {}
+
+  addPhotoToGallery() {
+    this.photoService.addNewToGallery();
+  }
 
 }
